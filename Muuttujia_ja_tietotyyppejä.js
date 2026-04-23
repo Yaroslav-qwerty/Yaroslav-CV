ed2d8aa7a74caca8fc2336b1ed5975dfe1b3292c
Yaroslav-qwerty <yk2329@edu.turku.fi>
2026-04-23 11:25:47 +0300

    Laskin

--- Muuttujia_ja_tietotyyppejä.js
+++ Muuttujia_ja_tietotyyppejä.js
@@ -20,3 +20,49 @@ function kerto() {
 function jako() {
     alert(d / a);
 }
+
+function summa2(luku1, luku2) {
+    document.getElementById("tulos").innerText = luku1 + luku2;
+}
+
+function vahennus2(luku1, luku2) {
+    document.getElementById("tulos").innerText = luku1 - luku2;
+}
+
+function kerto2(luku1, luku2) {
+    document.getElementById("tulos").innerText = luku1 * luku2;
+}
+
+function jako2(luku1, luku2) {
+    if (b == 0) {
+        document.getElementById("tulos").innerText = "Ei voi jakaa nollalla";
+    } else {
+        document.getElementById("tulos").innerText = luku1 / luku2;
+    }
+}
+
+
+
+function summa3() {
+    let luku11 = Number(document.getElementById("luku1").value);
+    let luku22 = Number(document.getElementById("luku2").value);
+    summa2(luku11, luku22);
+}
+
+function vahennys3() {
+    let luku11 = Number(document.getElementById("luku1").value);
+    let luku22 = Number(document.getElementById("luku2").value);
+    vahennus2(luku11, luku22);
+}
+
+function kerto3() {
+    let luku11 = Number(document.getElementById("luku1").value);
+    let luku22 = Number(document.getElementById("luku2").value);
+    kerto2(luku11, luku22);
+}
+
+function jako3() {
+    let luku11 = Number(document.getElementById("luku1").value);
+    let luku22 = Number(document.getElementById("luku2").value);
+    jako2(luku11, luku22);
+}
