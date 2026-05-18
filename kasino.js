4ff11d0a0b9d7badbef43214305170a74c5acb34
Yaroslav-qwerty <yk2329@edu.turku.fi>
2026-05-18 14:10:26 +0300

    Kasino1

--- kasino.js
+++ kasino.js
@@ -25,7 +25,6 @@ function Lotto() {
         return a - b;
     });
 
-
     let käyttäjänNum = [
         Number(document.getElementById("num1").value),
         Number(document.getElementById("num2").value),
