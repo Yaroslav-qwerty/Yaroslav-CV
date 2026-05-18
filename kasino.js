0425776ee6ec7311d747fe716f5032ced195ec2b
Yaroslav-qwerty <yk2329@edu.turku.fi>
2026-05-18 13:40:43 +0300

    Kasino

---
+++ kasino.js
@@ -0,0 +1,50 @@
+console.log("kasino-sivu avattu");
+
+function Noppa() {
+
+let luku = Math.floor(Math.random() * 6) + 1;
+
+document.getElementById("noppa").innerHTML =
+        "Noppa tulos: " + luku;
+}
+
+function Lotto() {
+
+    let numerot = [];
+
+    while (numerot.length < 7) {
+
+    let numero = Math.floor(Math.random() * 40) + 1;
+
+        if (!numerot.includes(numero)) {
+        numerot.push(numero);
+        }
+    }
+
+    numerot.sort(function(a, b) {
+        return a - b;
+    });
+
+
+    let käyttäjänNumerot = [
+        Number(document.getElementById("num1").value),
+        Number(document.getElementById("num2").value),
+        Number(document.getElementById("num3").value),
+        Number(document.getElementById("num4").value),
+        Number(document.getElementById("num5").value),
+        Number(document.getElementById("num6").value),
+        Number(document.getElementById("num7").value)
+    ];
+
+    let oikein = 0;
+
+    for (let numero of käyttäjänNumerot) {
+        if (numerot.includes(numero)) {
+            oikein++;
+        }
+    }
+
+    document.getElementById("lotto").innerHTML =
+        "Lotto numero: " + numerot.join(", ") +
+        "; Oikeita arvauksia: " + oikein;
+}
