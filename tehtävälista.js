7800e5126610e6c1cc471ce06095781369f62d6f
Yaroslav-qwerty <yk2329@edu.turku.fi>
2026-05-18 11:52:19 +0300

    Tehtävälista

--- tehtävälista.js
+++ tehtävälista.js
@@ -1,10 +1,24 @@
 console.log("tehtävälista-sivu avattu");
 
-let lista = ["Tehtävä 1", "Tehtävä 2"];
+let lisaa1 = ["Tehtävä 1", "Tehtävä 2"];
 
 function paivitaLista() {
   let ul = document.getElementById("lista1");
 
     ul.innerHTML = "";
-
+    for (let tehtava of lisaa1) {
+        let li = document.createElement("li");
+        li.innerHTML = tehtava;
+        ul.appendChild(li);
+    }
 }
+
+function lisaa() {
+    let input = document.getElementById("uusiTehtava");
+    let arvo = input.value;
+    lisaa1.push(arvo);
+    paivitaLista();
+    input.value = "";
+}
+
+paivitaLista();
